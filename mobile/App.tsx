234a275b8f2735204744065cc2fc5bb3d6ddf585
Izaías Araújo <izaiasousa@gmail.com>
2021-09-19 22:57:13 -0300

    chore: initial files from mobile app

---
+++ mobile/App.tsx
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+import useCachedResources from './src/hooks/useCachedResources';
+import useColorScheme from './src/hooks/useColorScheme';
+import Navigation from './src/navigation';
+
+import {NativeBaseProvider} from 'native-base';
+
+export default function App() {
+  const isLoadingComplete = useCachedResources();
+  const colorScheme = useColorScheme();
+
+  if (!isLoadingComplete) {
+    return null;
+  } else {
+    return (
+      <NativeBaseProvider>
+        <Navigation colorScheme={colorScheme} />
+        <StatusBar />
+      </NativeBaseProvider>
+    );
+  }
+}
